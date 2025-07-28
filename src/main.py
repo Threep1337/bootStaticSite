821b596fafd69f0201d447043156765c0d1e478f
threep <alangton1337@gmail.com>
2025-07-28 19:08:36 -0400

    fixed url bug

--- src/main.py
+++ src/main.py
@@ -6,12 +6,13 @@ import sys
 def main():
 
     basepath = "/"
-    if len(sys.argv) > 2:
+    if len(sys.argv) >= 2:
         basepath = sys.argv[1]
 
     #node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     #print(node)
 
+    print(f"Base Path is :{basepath}")
     copy_files_recursive('/home/threep/bootdev/bootStaticSite/static','/home/threep/bootdev/bootStaticSite/docs')
     #generate_page("/home/threep/bootdev/bootStaticSite/content/index.md","/home/threep/bootdev/bootStaticSite/template.html","/home/threep/bootdev/bootStaticSite/public/index.html")
     generate_pages_recursive("/home/threep/bootdev/bootStaticSite/content","/home/threep/bootdev/bootStaticSite/template.html","/home/threep/bootdev/bootStaticSite/docs",basepath)
